e5e489fdc23a77f02fa5d187b141c9d603624b56
Piotr Sobczak <piotrek246@gmail.com>
2018-03-26 21:05:25 +0200

    Creates user entity

---
+++ src/main/java/pl/sobczakpiotr/configs/AppConfig.java
@@ -0,0 +1,31 @@
+package pl.sobczakpiotr.request;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author Piotr Sobczak, created on 19-03-2018
+ */
+@Configuration
+@EnableTransactionManagement
+public class AppConfig {
+
+  @Autowired
+  Environment environment;
+
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName(environment.getProperty("spring.datasource.driverClassName"));
+    dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+    dataSource.setUsername(environment.getProperty("spring.datasource.user"));
+    dataSource.setPassword(environment.getProperty("jdbc.pass"));
+
+    return dataSource;
+  }
+}
---
+++ src/main/java/pl/sobczakpiotr/model/User.java
@@ -0,0 +1,8 @@
+package pl.sobczakpiotr.model;
+
+/**
+ * @author Piotr Sobczak, created on 19-03-2018
+ */
+public class User {
+
+}
